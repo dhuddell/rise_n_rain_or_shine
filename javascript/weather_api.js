a486af1d2b99093c00cfa056b6047d9cef96ca3f
dhuddell <dan.huddell@gmail.com>
2015-11-11 16:50:22 -0500

    Adds CRU to profiles and CR to pairs
    Adds Create Read Update to profiles
    Adds Create Read to weather_pairs
    Adds navigation through registration login/logout, profile CRU and..
    Adds weather_pairs display and creation

--- javascript/weather_api.js
+++ javascript/weather_api.js
@@ -10,6 +10,8 @@ var weather_api = {
     });
   },
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
   register: function register(credentials, callback) {
     this.ajax({
       method: 'POST',
@@ -30,7 +32,96 @@ var weather_api = {
     }, callback);
   },
 
+  logout: function logout(id, token, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/logout/' + user_id,
+      headers: {
+          Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    }, callback);
+  },
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+createProfile: function createProfile(profile, user_id, token, callback) {
+  this.ajax({
+    method: 'POST',
+    url: this.url + '/users/' + user_id + '/profile',
+    headers: {
+        Authorization: 'Token token=' + token
+    },
+    contentType: 'application/json',
+    data: JSON.stringify(profile),
+    dataType: 'json'
+  }, callback);
+},
+
+updateProfile: function updateProfile(profile, user_id, token, callback) {
+  this.ajax({
+    method: 'PATCH',
+    url: this.url + '/users/' + user_id + '/profile',
+    headers: {
+        Authorization: 'Token token=' + token
+    },
+    contentType: 'application/json',
+    data: JSON.stringify(profile),
+    dataType: 'json'
+  }, callback);
+},
+
+readProfile: function readProfile(user_id, token, callback) {
+  this.ajax({
+    method: 'GET',
+    url: this.url + '/users/' + user_id + '/profile',
+    headers: {
+      Authorization: 'Token token=' + token
+    },
+    dataType: 'json'
+
+  }, callback);
+},
+
+showPairs: function showPairs(token, callback) {
+  this.ajax({
+    method: 'GET',
+    url: this.url +  '/profile/weather_pairs',
+    headers: {
+      Authorization: 'Token token=' + token
+    },
+    dataType: 'json'
+
+  }, callback);
+},
+
+
+createWeatherPair: function createWeatherPair(weather_pair, token, callback){
+  this.ajax({
+    method: 'POST',
+    url: this.url + '/profile' + '/weather_pairs',
+    headers: {
+        Authorization: 'Token token=' + token
+    },
+    contentType: 'application/json',
+    data: JSON.stringify(weather_pair),    //NEED TO ADD PROFILE ID TO THIS
+    dataType: 'json'
+    }, callback)
+},
+
+
+
+// Call to forecast.io asking for weather object
+get_weather: function get_weather(id, callback) {
+    this.ajax({
+      method: 'GET',
+      url: 'https://api.forecast.io/forecast/0d2fae036c63eb41ba914a58600cb1ef/' + current_user.zip_code,
+      headers: {},
+      contentType: 'application/json; charset=utf-8'
+    }, callback);
+  },
+//////////////////////////////////////////////////////////////////////////////////////////
 
   //Authenticated api actions
   listGames: function (token, callback) {
@@ -42,17 +133,6 @@ var weather_api = {
       },
       dataType: 'json'
       }, callback);
-  },
-
-
-// Call to forecast.io asking for weather object
-get_weather: function get_weather(id, callback) {
-    this.ajax({
-      method: 'GET',
-      url: 'https://api.forecast.io/forecast/0d2fae036c63eb41ba914a58600cb1ef/' + current_user.zip_code,
-      headers: {},
-      contentType: 'application/json; charset=utf-8'
-    }, callback);
   },
 
   createGame: function (token, callback) {
